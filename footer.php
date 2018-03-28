8b81f359e344fa7e8306e7b721758bea0e4eb111
Magnus Schjølberg <37403247+Magssch@users.noreply.github.com>
2018-03-28 20:37:58 +0200

    Parallax gallore

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
+<div class="bg_img bottom">
+  <div class="caption">
+    <span class="border">Follow us for more:</span><br/><br/><br/><br/>
+    <img src="img/fb.png" class="some" />
+    <img src="img/tw.png" class="some" />
+    <img src="img/ig.png" class="some" />
+    <img src="img/sc.png" class="some" />
+    <img src="img/gh.png" class="some" />
+  </div>
+</div>
 <div class="footer">
 © Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 </div>
